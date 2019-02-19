b6bef0b1642fba75438db0038c743a0b0328b07c
Charlotte Godley <charlotte@dotmesh.com>
2019-02-19 15:59:01 +0000

    frontend-ng#360: beginnings of infrastructure and fork changes

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 ensure_python(('2.7', '>=3.4'))
 
 # the name of the project
-name = 'nbdime'
+name = 'nbdime-dotscience'
 version = get_version(pjoin(name, '_version.py'))
 
 # Some paths
@@ -77,9 +77,7 @@ with open(pjoin(here, 'README.md')) as f:
 
 setup_args = dict(
     name            = name,
-    description     = "Diff and merge of Jupyter Notebooks",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    description     = "Diff and merge of Jupyter Notebooks, styled for dotscience",
     version         = version,
     scripts         = glob(pjoin('scripts', '*')),
     cmdclass        = cmdclass,
