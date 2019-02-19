1db4decbfd8f48fb90facb832c48224efd61ee80
Charlotte Godley <charlotte@dotmesh.com>
2019-02-19 19:24:09 +0000

    frontend-ng#360: use virtualenv as a workaround -_-

---
+++ release.sh
@@ -0,0 +1,7 @@
+pip3 install --upgrade virtualenv
+virtualenv -p python3 nbdime
+. nbdime/bin/activate
+python3 setup.py sdist bdist_wheel
+docker build -t quay.io/dotmesh/nbdime:$1 .
+docker login -u $QUAY_USER -p $QUAY_PASSWORD $RELEASE_DOCKER_REGISTRY
+docker push quay.io/dotmesh/nbdime:$1
