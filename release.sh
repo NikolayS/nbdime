7d25828e02af875efb3e7b52f852efc8acdf006e
Charlotte Godley <charlotte@dotmesh.com>
2019-02-20 10:46:39 +0000

    frontend-ng#360: somehow, a working docker build!

--- release.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get -y install python3-pip
-sudo apt-get -y install curl && sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - && sudo apt-get -y install build-essential nodejs
-pip3 install --upgrade virtualenv
-virtualenv -p python3 nbdime
-. nbdime/bin/activate
-python3 setup.py sdist bdist_wheel
-docker build -t quay.io/dotmesh/nbdime:$1 .
-docker login -u $QUAY_USER -p $QUAY_PASSWORD $RELEASE_DOCKER_REGISTRY
-docker push quay.io/dotmesh/nbdime:$1
