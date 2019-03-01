5c36ef5c43e931818e0f7d5a7e1e1fd40e820a32
Charlotte Godley <charlotte@dotmesh.com>
2019-03-01 16:22:22 +0000

    local-stack#8: rip out "file-like" object uploading, add in prefix
    This a) stops people from doing anything that's not a valid gateway url
    b) allows us (or anyone else who should use this image) to reconfigure it should that change

--- nbdime/webapp/nbdimeserver.py
+++ nbdime/webapp/nbdimeserver.py
@@ -43,6 +43,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 static_path = os.path.join(here, 'static')
 template_path = os.path.join(here, 'templates')
 
+S3_URL_PREFIX = os.environ.get("S3_URL_PREFIX", 'http://gateway:8700')
+
 
 class NbdimeHandler(IPythonHandler):
     def initialize(self, **params):
@@ -86,7 +88,7 @@ class NbdimeHandler(IPythonHandler):
                 path = arg
             else:
                 path = os.path.join(self.curdir, arg)
-                r = requests.get(arg)
+                r = requests.get(S3_URL_PREFIX + arg)
 
             # Let nbformat do the reading and validation
             if os.path.exists(path):
@@ -219,10 +221,6 @@ class ApiDiffHandler(NbdimeHandler, APIHandler):
     def get_notebook_argument(self, argname):
         if 'difftool_args' in self.params:
             arg = self.params['difftool_args'][argname]
-            if not isinstance(arg, string_types):
-                # Assume arg is file-like
-                arg.seek(0)
-                return nbformat.read(arg, as_version=4)
             return self.read_notebook(arg)
         return super(ApiDiffHandler, self).get_notebook_argument(argname)
 
